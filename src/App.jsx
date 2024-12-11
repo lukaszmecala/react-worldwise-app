0fdb393ba24c488dde8812e7445b29e8df002f2b
lukaszmecala <lukaszfronia99@gmail.com>
2024-12-11 22:52:49 +0100

    created params for link to stored data in url

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -49,6 +50,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
